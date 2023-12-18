d5f1d29cddb017bd71e755a1915940f007dfc8c6
Matheus Elegância <73546732+MathMaster697@users.noreply.github.com>
2023-12-18 19:53:50 -0300

    Ok

---
+++ 01_basico-de-python/06_usando_variaveis_em_strings.py
@@ -0,0 +1,8 @@
+# primeiro_nome = "matheus"
+# ultimo_nome = "elegância"
+# nome_completo = f"{primeiro_nome} {ultimo_nome}"
+# print(nome_completo)
+
+nome = "Davi"
+idade = "40"
+frase = f"Olá, meu nome é (nome) e tenho (idade) anos."
